81688d3e1aec0e25ffc67f0e4dd0be19b0a105f8
dnizam <41112906+dnizam@users.noreply.github.com>
2020-06-26 17:31:59 +0530

    Update test-selenium.py

--- selenium-python/test-selenium.py
+++ selenium-python/test-selenium.py
@@ -28,9 +28,9 @@ wait = WebDriverWait( driver, 5 )
 
 #validate if the entry is updated in database
 driver.get("http://104.197.190.255:8082/user/get-by-email?email="+"test"+str(rand)+"@test.com")
-wait = WebDriverWait( driver, 5 )
+wait = WebDriverWait( driver, 8 )
 element = driver.find_element_by_xpath("/html/body")
-msgText = element.get_attribute('value')
+msgText = element.get_attribute("value")
 print(msgText)
 assert msgText !=  "User not found"
 driver.close()
