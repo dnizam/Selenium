35cc321ae2f3bb7fd885284f5c2b0387cfb5c7d0
dnizam <41112906+dnizam@users.noreply.github.com>
2020-06-26 17:27:35 +0530

    Update test-selenium.py

--- selenium-python/test-selenium.py
+++ selenium-python/test-selenium.py
@@ -21,11 +21,16 @@ element = driver.find_element_by_xpath("/html/body/form/input[1]")
 element.send_keys("test"+str(rand))
 element = driver.find_element_by_xpath("/html/body/form/input[3]")
 element.send_keys("test"+str(rand)+"@test.com")
-print(driver.title)
+submit = driver.find_element_by_xpath("/html/body/form/input[3]")
+submit.click()
+wait = WebDriverWait( driver, 5 )
+#print(driver.title)
 
 #validate if the entry is updated in database
-#driver.get("http://104.197.190.255:8082/user/get-by-email?email="+"test"+random+"@test.com")
-#element = driver.find_element_by_xpath("/html/body")
-#msgText = element.get_attribute('value')
-#assert msgText !=  "User not found"
+driver.get("http://104.197.190.255:8082/user/get-by-email?email="+"test"+random+"@test.com")
+wait = WebDriverWait( driver, 5 )
+element = driver.find_element_by_xpath("/html/body")
+msgText = element.get_attribute('value')
+print(msgText)
+assert msgText !=  "User not found"
 driver.close()
