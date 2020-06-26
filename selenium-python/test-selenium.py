0fe0247e4ac40d37c7227b3df53a1d89ef22b0f1
dnizam <41112906+dnizam@users.noreply.github.com>
2020-06-26 17:49:31 +0530

    Update test-selenium.py

--- selenium-python/test-selenium.py
+++ selenium-python/test-selenium.py
@@ -19,7 +19,7 @@ driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
                          )
 driver.get("http://35.239.244.72:8085/getForm")
 wait = WebDriverWait( driver, 8 )
-element = driver.find_element_by_xpath("/html/body/form/input[1]")
+element = driver.find_element_by_name("employeeName")
 element.send_keys("test"+str(rand))
 element = driver.find_element_by_xpath("/html/body/form/input[3]")
 element.send_keys("test"+str(rand)+"@test.com")
