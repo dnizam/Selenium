6b5c7dd5e0cba17edfb2009dc4f3686db4da7bf4
dnizam <41112906+dnizam@users.noreply.github.com>
2020-06-26 16:59:20 +0530

    Update test-selenium.py

--- selenium-python/test-selenium.py
+++ selenium-python/test-selenium.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from random import seed
+from random import random
+# seed random number generator
+seed(1)
+rand = random()
 CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
 WINDOW_SIZE = "1920,1080"
 chrome_options = Options()
@@ -9,6 +14,15 @@ chrome_options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
                           chrome_options=chrome_options
                          )
-driver.get("https://www.google.com")
-print(driver.title)
+driver.get("http://35.239.244.72:8085/getForm")
+element = driver.find_element_by_name("employeeName")
+element.send_keys("test"+rand)
+element = driver.find_element_by_xpath("/html/body/form/input[3]")
+element.send_keys("test"+rand+"@test.com")
+
+#validate if the entry is updated in database
+driver.get("http://104.197.190.255:8082/user/get-by-email?email="+"test"+random+"@test.com")
+element = driver.find_element_by_xpath("/html/body")
+msgText = element.get_attribute('value')
+assert msgText !=  "User not found"
 driver.close()
