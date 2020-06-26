b4a01691c7692023adf0dd875418bfd8f3e2bbc1
dnizam <41112906+dnizam@users.noreply.github.com>
2020-06-26 17:23:22 +0530

    Update test-selenium.py

--- selenium-python/test-selenium.py
+++ selenium-python/test-selenium.py
@@ -18,9 +18,9 @@ driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
 driver.get("http://35.239.244.72:8085/getForm")
 wait = WebDriverWait( driver, 5 )
 element = driver.find_element_by_xpath("/html/body/form/input[1]")
-element.send_keys("test"+rand)
-#element = driver.find_element_by_xpath("/html/body/form/input[3]")
-#element.send_keys("test"+rand+"@test.com")
+element.send_keys("test"+str(rand))
+element = driver.find_element_by_xpath("/html/body/form/input[3]")
+element.send_keys("test"+str(rand)+"@test.com")
 print(driver.title)
 
 #validate if the entry is updated in database
